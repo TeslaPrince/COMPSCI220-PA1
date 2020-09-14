76cc4b10bb6501047e864816edfaaf0cc20dc9e9
Tesla Prince <57016635+TeslaPrince@users.noreply.github.com>
2020-09-13 20:21:28 -0500

    Add files via upload

---
+++ BalancedParenthesis.java
@@ -0,0 +1,15 @@
+package PA1;
+
+public class BalancedParenthesis {
+
+	private char[] expression;
+
+	public BalancedParenthesis(char[] expression) { // complete the constructor
+	}
+
+	public boolean isBalanced() { // complete this method
+	}
+
+	public String toString() { // complete this method
+	}
+}
---
+++ Stack.java
@@ -0,0 +1,53 @@
+package PA1;
+
+import java.lang.*;
+
+public class Stack {
+	char[] stackArray;
+	int maxStackSize;
+	int topOfStack = maxStackSize - maxStackSize - 1;
+	public Stack(int maxStackSize){
+		maxStackSize = this.maxStackSize;
+		stackArray= new char[maxStackSize];
+	}
+	void push(char val) {
+		if (topOfStack == maxStackSize - 1) {
+			System.out.println("Cannot push! Stack is full");
+		}
+		else {
+			topOfStack++;
+			stackArray[topOfStack] = val;
+		}
+	}
+	char pop() {
+		if(topOfStack == -1) {
+			System.out.println("Cannot pop! Stack is empty");
+			return '\0';
+		}
+		else {
+			char m = stackArray[topOfStack];
+			topOfStack--;
+			return m;
+		}
+	}
+	char peek() {
+		if(topOfStack == -1) {
+			System.out.println("Cannot peek! Stack is empty");
+			return '\0';
+		}
+		else {
+			return stackArray[topOfStack];
+		}
+	}
+	int getSize() {
+		return (topOfStack +1);
+	}
+	public String toString() {
+		String Array = "[";
+		for (int i = 0; i <= topOfStack; i++) {
+			Array = Array + stackArray[i] + ",";
+		}
+		Array = Array + "]";
+		return Array;
+	}
+}
